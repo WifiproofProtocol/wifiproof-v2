76c23190894f2feac41e1ec9333ef432bf2400dc
wamimi <nellycyberpro@gmail.com>
2026-02-16 21:51:34 +0300

    refactor(contracts): support Foundry keystore in deploy script
    Makes DEPLOYER_PRIVATE_KEY optional via vm.envOr so the script works
    with --account keystore aliases. Passes new cbAttester and
    cbVerifiedAccountSchema params to the WiFiProof constructor.

--- packages/contracts/script/Deploy.s.sol
+++ packages/contracts/script/Deploy.s.sol
@@ -10,22 +10,44 @@ import {HonkVerifier} from "../src/Verifier.sol";
 /// @notice Deploys the Honk verifier + WiFiProof gatekeeper contract
 contract Deploy is Script {
     function run() external {
-        uint256 deployerKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-
         address eas = vm.envAddress("EAS_ADDRESS");
         address cbIndexer = vm.envAddress("CB_INDEXER_ADDRESS");
+        address cbAttester = vm.envAddress("CB_ATTESTER");
+        bytes32 cbVerifiedAccountSchema = vm.envBytes32("CB_VERIFIED_ACCOUNT_SCHEMA");
         address ipSigner = vm.envAddress("IP_SIGNER_ADDRESS");
 
-        address owner = vm.envOr("OWNER_ADDRESS", vm.addr(deployerKey));
+        uint256 deployerKey = vm.envOr("DEPLOYER_PRIVATE_KEY", uint256(0));
+        address owner = vm.envOr("OWNER_ADDRESS", address(0));
         bytes32 schema = vm.envOr("WIFIPROOF_SCHEMA", bytes32(0));
 
-        vm.startBroadcast(deployerKey);
+        address deployer;
+        if (deployerKey != 0) {
+            deployer = vm.addr(deployerKey);
+            if (owner == address(0)) {
+                owner = deployer;
+            }
+            vm.startBroadcast(deployerKey);
+        } else {
+            vm.startBroadcast();
+            deployer = msg.sender;
+            if (owner == address(0)) {
+                owner = deployer;
+            }
+        }
 
         HonkVerifier verifier = new HonkVerifier();
-        WiFiProof wifiproof = new WiFiProof(eas, address(verifier), cbIndexer, ipSigner, owner);
+        WiFiProof wifiproof = new WiFiProof(
+            eas,
+            address(verifier),
+            cbIndexer,
+            cbAttester,
+            cbVerifiedAccountSchema,
+            ipSigner,
+            owner
+        );
 
         if (schema != bytes32(0)) {
-            if (owner == vm.addr(deployerKey)) {
+            if (owner == deployer) {
                 wifiproof.setSchema(schema);
             } else {
                 console2.log("Skipping setSchema: OWNER_ADDRESS is not deployer.");
